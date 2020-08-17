493f9847e92427df15ca3c96a7fd2a294ca9b68f
Sukhwinder Singh <SS137@atcltd.atc.org.nz>
2020-08-18 11:56:41 +1200

    Add project files.

---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RussianRoulette;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        RouletteGame test = new RouletteGame();
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+            test.startChamber = test.spinChamber();
+            test.bulletChamber = test.spinChamber();
+        }
+
+        [TestMethod]
+        public void FireMethodAsync()
+        {
+            test.startChamber = test.spinChamber();
+            test.bulletChamber = test.spinChamber();
+            int currentChamber = test.GunFire(test.numPlayers, test.startChamber, test.bulletChamber);
+
+        }
+    }
+}
